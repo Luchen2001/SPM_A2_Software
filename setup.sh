8a99bdf97b18c5e508c5d63284ed0eda3abc5756
Luchen2001 <110806242+Luchen2001@users.noreply.github.com>
2023-04-22 23:59:23 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@
 brew install node
 
 # Set up React app
-cd ~/frontend
+cd ./frontend
 npm install
 npm run build
 
 # Install required packages
-cd ~/backend
+cd ./backend
 pip3 install fastapi
 pip3 install uvicorn[standard]
