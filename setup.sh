6d361e8bddd8f9111deff7e3b842e3bac519f4b0
Luchen2001 <110806242+Luchen2001@users.noreply.github.com>
2023-04-22 23:55:25 +1000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Install required packages
+brew install node
+
+# Set up React app
+cd ~/frontend
+npm install
+npm run build
+
+# Install required packages
+cd ~/backend
+pip3 install fastapi
+pip3 install uvicorn[standard]
